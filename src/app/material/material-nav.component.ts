126150031332333552e3623ea009e3023007ac64
Jason Stone <jason.stone@nextgen.net>
2018-05-26 21:52:42 +1000

    more material examples; update npm dependencies

--- src/app/material/material-nav.component.ts
+++ src/app/material/material-nav.component.ts
@@ -5,10 +5,10 @@ import { ViewPlatformService } from '../../lib/common';
 @Component({
   selector: 'ww-material-nav',
   template: `
-    <nav class="nav flex-column">
+    <nav class="mat-nav-list">
       <a
         *ngFor="let section of sections"
-        class="nav-link"
+        class="list-item"
         (click)="scrollTo('material:' + section.id)"
       >{{section.label}}</a>
     </nav>
@@ -20,6 +20,38 @@ export class MaterialNavComponent {
   sections = [
     {id: 'autocomplete', label: 'Autocomplete'},
     {id: 'checkbox', label: 'Checkbox'},
+    {id: 'datepicker', label: 'Datepicker'},
+    {id: 'formfield', label: 'Form field'},
+    {id: 'input', label: 'Input'},
+    {id: 'radiobutton', label: 'Radio button'},
+    {id: 'select', label: 'Select'},
+    {id: 'slider', label: 'Slider'},
+    {id: 'slidetoggle', label: 'Slide toggle'},
+    {id: 'menu', label: 'Menu'},
+    {id: 'sidenav', label: 'Sidenav'},
+    {id: 'toolbar', label: 'Toolbar'},
+    {id: 'card', label: 'Card'},
+    {id: 'divider', label: 'Divider'},
+    {id: 'expansionpanel', label: 'Expansion Panel'},
+    {id: 'gridlist', label: 'Grid list'},
+    {id: 'list', label: 'List'},
+    {id: 'stepper', label: 'Stepper'},
+    {id: 'tabs', label: 'Tabs'},
+    {id: 'tree', label: 'Tree'},
+    {id: 'button', label: 'Button'},
+    {id: 'buttontoggle', label: 'Button toggle'},
+    {id: 'badge', label: 'Badge'},
+    {id: 'chips', label: 'Chips'},
+    {id: 'icon', label: 'Icon'},
+    {id: 'progressspinner', label: 'Progress spinner'},
+    {id: 'progressbar', label: 'Progress bar'},
+    {id: 'bottomsheet', label: 'Bottom Sheet'},
+    {id: 'dialog', label: 'Dialog'},
+    {id: 'snackbar', label: 'Snackbar'},
+    {id: 'tooltip', label: 'Tooltip'},
+    {id: 'paginator', label: 'Paginator'},
+    {id: 'sortheader', label: 'Sort header'},
+    {id: 'table', label: 'Table'}
   ];
 
   constructor(private viewPlatformService: ViewPlatformService) {}
--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ww-material',
@@ -17,6 +17,36 @@ import { FormControl } from '@angular/forms';
       width: 100%;
     }
 
+    .example-container {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .example-container > * {
+      width: 100%;
+    }
+
+    .example-radio-group {
+      display: inline-flex;
+      flex-direction: column;
+    }
+
+    .example-radio-button {
+      margin: 5px;
+    }
+
+    .example-selected-value {
+      margin: 15px 0;
+    }
+
+    .mat-slider-horizontal {
+      width: 300px;
+    }
+
+    .mat-slider-vertical {
+      height: 300px;
+    }
+
     .example-h2 {
       margin: 10px;
     }
@@ -29,11 +59,49 @@ import { FormControl } from '@angular/forms';
     }
 
     .example-margin {
-      margin: 0 10px;
+      margin: 10px;
+    }
+
+    .example-container {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: #eee;
+    }
+
+    .example-icon {
+      padding: 0 14px;
+    }
+
+    .example-spacer {
+      flex: 1 1 auto;
+    }
+
+    .example-card {
+      max-width: 400px;
+    }
+
+    .example-header-image {
+      background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg');
+      background-size: cover;
+    }
+
+    .mat-list-icon {
+      color: rgba(0, 0, 0, 0.54);
+    }
+
+    .demo-tab-group {
+      border: 1px solid #e8e8e8;
+    }
+
+    .demo-tab-content {
+      padding: 16px;
     }
   `]
 })
-export class MaterialComponent {
+export class MaterialComponent implements OnInit {
   myControl: FormControl = new FormControl();
 
   options = [
@@ -42,8 +110,87 @@ export class MaterialComponent {
     'Three'
   ];
 
-  checked = false;
   indeterminate = false;
   labelPosition = 'after';
+
+  favoriteSeason: string;
+
+  seasons = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
+
+
+  autoTicks = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 1;
+
+  color = 'accent';
+  checked = false;
   disabled = false;
+
+  panelOpenState = false;
+
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
+
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 }
